c57d68245b83ec7d30d6cbc19e55771fe97b0f5c
Yiwen Wang <yiwwan@microsoft.com>
2022-09-13 12:24:24 +0000

    Update font script

--- powershell/SetupFont.ps1
+++ powershell/SetupFont.ps1
@@ -5,14 +5,13 @@ EnsureAdminPrivileges;
 function DownloadFromGithubLatestRelease(
     [string] $organization,
     [string] $repo,
-    [string] $linkPattern,
+    [string] $namePattern,
     [string] $localFile) {
-    $latestReleasePage = Invoke-WebRequest https://github.com/$organization/$repo/releases/latest
-    $latestReleaseHref = ($latestReleasePage.Links | Where-Object { $_.href -match "$linkPattern" })[0].href
-    if (-not $latestReleaseHref) {
-        throw "Latest release link with pattern $linkPattern not found.";
+    $latestReleases = Invoke-WebRequest "https://api.github.com/repos/$organization/$repo/releases/latest" -UseBasicParsing | ConvertFrom-Json
+    $latestReleaseDownloadLink = ($latestReleases.assets | Where-Object { $_.name -match "$namePattern" })[0].browser_download_url
+    if (-not $latestReleaseDownloadLink) {
+        throw "Latest release link with pattern $namePattern not found.";
     }
-    $latestReleaseDownloadLink = "https://github.com$latestReleaseHref";
     Write-Host "Downloading found release $latestReleaseDownloadLink to $localFile...";
     Invoke-WebRequest $latestReleaseDownloadLink -OutFile $localFile
     Write-Host "Done.";
@@ -28,14 +27,22 @@ $code = @'
     public static class FontUtil {
         [DllImport("gdi32.dll")]
         public static extern int AddFontResource(string lpFilename);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int WriteProfileString(string lpszSection, string lpszKeyName, string lpszString);
     }
 '@
 Add-Type $code
+
+$WM_FONTCHANGE = 0x001D;
+$HWND_BROADCAST = 0xffff;
 
 function InstallFont(
     [string] $fontFile
 ) {
     $fontFileName = Split-Path -Leaf $fontFile;
+    $fontName = [System.IO.Path]::GetFileNameWithoutExtension($fontFile)
     $fontInstallPath = "C:\Windows\Fonts\$fontFileName";
     if (Test-Path $fontInstallPath) {
         Write-Host "Font $fontFileName already installed.";
@@ -43,7 +50,9 @@ function InstallFont(
     }
     Copy-Item $fontFile $fontInstallPath;
     Write-Host "Installing $fontFileName...";
-    [FontUtil]::AddFontResource($fontInstallPath) | Out-Null
+    [FontUtil]::AddFontResource($fontInstallPath) | Out-Null;
+    [FontUtil]::WriteProfileString("fonts", $fontName, $fontFileName) | Out-Null;
+    [FontUtil]::SendMessage($HWND_BROADCAST, $WM_FONTCHANGE, 0, 0) | Out-Null;
     Write-Host "Done.";
 }
 
@@ -53,13 +62,13 @@ try {
     if (Test-Path $tempFolder) {
         Remove-Item -Recurse $tempFolder;
     }
-    New-Item -ItemType Directory $tempFolder;
+    [void](New-Item -ItemType Directory $tempFolder);
 
     DownloadFromGithubLatestRelease ryanoasis nerd-fonts CascadiaCode.zip "$tempFolder/CascadiaCodeNF.zip"
     DownloadFromGithubLatestRelease microsoft cascadia-code CascadiaCode-.*.zip "$tempFolder/CascadiaCode.zip"
     Expand-Archive "$tempFolder/CascadiaCodeNF.zip" "$tempFolder/CascadiaCodeNF";
     Expand-Archive "$tempFolder/CascadiaCode.zip" "$tempFolder/CascadiaCode";
-    Get-ChildItem "$tempFolder/CascadiaCodeNF" -Recurse -Filter "*.ttf" | Where-Object {
+    Get-ChildItem "$tempFolder/CascadiaCodeNF" -Recurse -Filter "*.otf" | Where-Object {
         $_.Name.Contains("Windows")
     } | ForEach-Object {
         InstallFont $_.FullName;
