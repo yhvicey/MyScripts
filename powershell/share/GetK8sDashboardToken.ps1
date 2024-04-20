e5268023822cce53ca45ffdd21725a9003cac93b
Vicey Wang <vicey@live.com>
2024-04-21 00:30:29 +0800

    Update token acquiring

--- powershell/share/GetK8sDashboardToken.ps1
+++ powershell/share/GetK8sDashboardToken.ps1
@@ -1,12 +1,11 @@
 function GetK8sDashboardToken(
     [ValidateSet(
-        "web3datadri-token"
+        "adstool"
     )]
-    [string]$TokenNamePattern = "web3datadri-token"
+    [string]$ServiceAccount = "adstool"
 ) {
-    EnsureNotNullOrEmpty $TokenNamePattern
-    $tokenName = ((kubectl get secret -n kube-system | Where-Object { $_ -match $TokenNamePattern }) -split " ")[0]
-    $token = kubectl get secret -n kube-system "$tokenName" -o jsonpath='{.data.token}'
-    $token | DecodeFromBase64 | Set-Clipboard
-    Write-Host "Token for $TokenNamePattern have been copied to clipboard."
+    EnsureNotNullOrEmpty $ServiceAccount
+    $token = kubectl create token $ServiceAccount -n kube-system
+    $token | Set-Clipboard
+    Write-Host "Token for $ServiceAccount have been copied to clipboard."
 }
