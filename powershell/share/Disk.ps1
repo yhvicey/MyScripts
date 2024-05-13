4b346410f06ec1365237cc2159926b15ab6706f4
Vicey Wang <vicey@live.com>
2024-05-13 13:47:07 +0800

    Adjust sdu

--- powershell/share/Disk.ps1
+++ powershell/share/Disk.ps1
@@ -1,21 +1,3 @@
-function GetDiskUsage([string]$Directory = ".") {
-    Get-ChildItem $Directory | ForEach-Object {
-        $folder = $_;
-        Get-ChildItem -r $_.FullName | Measure-Object -Property Length -Sum | Select-Object @{
-            Name       = "Name";
-            Expression = {
-                $folder
-            }
-        }, @{
-            Name       = "Length";
-            Expression = {
-                Get-FileSize $_.Sum;
-            }
-        }, Sum
-    } | Sort-Object -Property Sum -Descending | Select-Object Name, Length
-}
-Set-Alias "sdu" "GetDiskUsage";
-
 function GetFileSize([long]$fileSize) {
     switch ($fileSize) {
         { $_ -gt 1tb } {
@@ -36,3 +18,21 @@ function GetFileSize([long]$fileSize) {
     }
 }
 Set-Alias "fsize" "GetFileSize";
+
+function GetDiskUsage([string]$Directory = ".") {
+    Get-ChildItem $Directory | ForEach-Object {
+        $folder = $_;
+        Get-ChildItem -r $_.FullName | Measure-Object -Property Length -Sum | Select-Object @{
+            Name       = "Name";
+            Expression = {
+                $folder
+            }
+        }, @{
+            Name       = "Length";
+            Expression = {
+                GetFileSize $_.Sum;
+            }
+        }, Sum
+    } | Sort-Object -Property Sum -Descending | Select-Object Name, Length
+}
+Set-Alias "sdu" "GetDiskUsage";
