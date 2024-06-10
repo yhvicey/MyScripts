e3a33e57da1c7d49e46abdc236967e9ff296ff5a
Vicey Wang <vicey@live.com>
2024-06-10 21:55:02 +0800

    - Install tools to default location - Add git

--- powershell/SetupTools.ps1
+++ powershell/SetupTools.ps1
@@ -20,16 +20,7 @@ if (-not $SkipChocolatey) {
     # Install choco.exe if not installed
     $chocoExe = (Get-Command "choco" -ErrorAction SilentlyContinue).Path;
     if (($null -eq $chocoExe) -or -not (Test-Path $chocoExe)) {
-        $chocolateyInstall = [System.Environment]::GetEnvironmentVariable("ChocolateyInstall", "Machine")
-        if ([string]::IsNullOrEmpty($chocolateyInstall)) {
-            $chocolateyInstall = "$ToolsFolder\chocolatey";
-        }
-        else {
-            Write-Host "Picking ChocolateyInstall from existing environment variable"
-        }
-        Write-Host "choco.exe not found, install it to $chocolateyInstall...";
-        [System.Environment]::SetEnvironmentVariable("ChocolateyInstall", $chocolateyInstall, "Machine");
-        $env:ChocolateyInstall = $chocolateyInstall;
+        Write-Host "choco.exe not found, install it...";
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
         $chocoExe = (Get-Command "choco").Path;
