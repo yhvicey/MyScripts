b73cff4d3d507c275c4e73bf85ddde4853f453ab
Vicey Wang <vicey@live.com>
2024-08-05 19:27:11 +0800

    Update

--- powershell/share/GetK8sDashboardToken.ps1
+++ powershell/share/GetK8sDashboardToken.ps1
@@ -2,10 +2,11 @@ function GetK8sDashboardToken(
     [ValidateSet(
         "adstool"
     )]
-    [string]$ServiceAccount = "adstool"
+    [string]$ServiceAccount = "adstool",
+    [string]$Duration = "24h"
 ) {
     EnsureNotNullOrEmpty $ServiceAccount
-    $token = kubectl create token $ServiceAccount -n kube-system
+    $token = kubectl create token $ServiceAccount -n kube-system --duration $Duration
     $token | Set-Clipboard
     Write-Host "Token for $ServiceAccount have been copied to clipboard."
 }
