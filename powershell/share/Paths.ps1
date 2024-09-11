fbe05307f7424e3889a4aa5ea1e61f5e80e4614f
Vicey Wang <vicey@live.com>
2024-09-11 13:25:45 +0800

    Refactor Paths.ps1 to handle missing VS installation path

--- powershell/share/Paths.ps1
+++ powershell/share/Paths.ps1
@@ -5,7 +5,12 @@ AddToPath "$global:ToolsFolder\python3\Scripts";
 
 $vswhereCmd = Get-Command "vswhere" -ErrorAction SilentlyContinue
 if ($vswhereCmd) {
-    $installationPath = (& $vswhereCmd.Source | Where-Object { $_ -match "^installationPath: .*" }).Replace("installationPath: ", "")
+    $installationPath = & $vswhereCmd.Source -all | Where-Object { $_ -match "^installationPath: .*" }
+    if (-not $installationPath) {
+        Write-Debug "VS installation path not found"
+        return
+    }
+    $installationPath = $installationPath.Replace("installationPath: ", "")
     # devenv
     $idePath = "$installationPath\Common7\IDE\"
     AddToPath "$idePath"
