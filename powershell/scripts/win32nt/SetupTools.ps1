2b919009946d287dd7d42d79db166548d08dec08
Vicey Wang <vicey@live.com>
2026-01-15 14:34:58 -0800

    feat: Update SetupTools.ps1 to include EditWingetList and EditChocolateyList parameters; modify winget.tools to correct JetBrains IntelliJ IDEA entry

--- powershell/scripts/win32nt/SetupTools.ps1
+++ powershell/scripts/win32nt/SetupTools.ps1
@@ -1,6 +1,8 @@
 param(
     [switch]$SkipWinget = $false,
-    [switch]$SkipChocolatey = $false
+    [switch]$SkipChocolatey = $false,
+    [switch]$EditWingetList = $false,
+    [switch]$EditChocolateyList = $false
 )
 
-& "$PSScriptRoot/../../SetupTools.ps1" -SkipWinget:$SkipWinget -SkipChocolatey:$SkipChocolatey
+& "$PSScriptRoot/../../SetupTools.ps1" -SkipWinget:$SkipWinget -SkipChocolatey:$SkipChocolatey -EditWingetList:$EditWingetList -EditChocolateyList:$EditChocolateyList
